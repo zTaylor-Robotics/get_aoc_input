6ccb3bb61b2d065f386eb96b23731f660beeea30
Zachary <z.taylor@hiwin.us>
2023-12-29 23:35:43 -0600

    update

--- get_input.sh
+++ get_input.sh
@@ -1,5 +1,5 @@
 cmake -S /get_aoc_input/ -B /get_aoc_input/bin -G "Ninja Multi-Config"
 
-ninja -f /get_aoc_input/bin/build-Release.ninja
+ninja -C /get_aoc_input/bin/ -f build-Release.ninja
 
 ln -s /get_aoc_input/bin/Release/get_input /usr/bin/get_input
